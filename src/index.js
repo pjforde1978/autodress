29705af181d4b6da678151b15d7adbc9783bcde1
Pete Forde <hello@peteforde.com>
2019-01-08 17:49:54 -0500

    now with postal codes

--- src/index.js
+++ src/index.js
@@ -4,29 +4,20 @@ import Geosuggest from "react-geosuggest";
 import "./index.css";
 
 export default class App extends React.Component {
-  /**
-   * Render the example app
-   */
+  onSuggestSelect(suggest) {
+    console.log(suggest);
+    console.log(suggest.gmaps.address_components.slice(-1)[0].long_name);
+  }
+
   render() {
-    const google = window.google;
     return (
       <div>
         <h1>Where to?</h1>
         <Geosuggest
           onSuggestSelect={this.onSuggestSelect}
-          location={new google.maps.LatLng(43.5888128, -79.5492352)}
-          radius="20"
         />
       </div>
     );
-  }
-
-  /**
-   * When a suggest got selected
-   * @param  {Object} suggest The suggest
-   */
-  onSuggestSelect(suggest) {
-    console.log(suggest);
   }
 }
 
